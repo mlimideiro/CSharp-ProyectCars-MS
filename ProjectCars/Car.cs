aa52f4727916cd20e4f7daea43434cf01fbbb5f7
Marcelo <mlimideiro@gmail.com>
2021-08-30 19:45:38 -0300

    Armando la lista de autos.

--- ProjectCars/Car.cs
+++ ProjectCars/Car.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace ProjectCars
 {
-    class Car
+    public class Car
     {
+        public string id { get; set; }
+        public string model { get; set; }
+        public int doorsQuantity { get; set; }
+        public string color { get; set; }
+        public bool gearBox { get; set; }
+
     }
 }
